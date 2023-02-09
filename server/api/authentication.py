b909ff921902c73d9c99ebafd823843523f50931
Hacknerr <andrgart@stud.ntnu.no>
2023-02-09 10:49:07 +0100

    Partial folder structure for the repository created. Also initialized and implemented API authentication logic.

---
+++ server/api/authentication.py
@@ -0,0 +1,32 @@
+import requests
+from server.api.credentials import config
+
+
+# retrieves an access token from a token URL using client credentials.
+def get_token():
+    if not config['client_id']:
+        raise ValueError('client_id must be set in credentials.py')
+
+    if not config['client_secret']:
+        raise ValueError('client_secret must be set in credentials.py')
+
+    # makes the request
+    req = requests.post(config['token_url'],
+                        data={
+                            'grant_type': 'client_credentials',
+                            'client_id': config['client_id'],
+                            'client_secret': config['client_secret'],
+                            'scope': 'ais'
+                        },
+                        headers={'content-type': 'application/x-www-form-urlencoded'})
+
+    req.raise_for_status()
+    print('Token request successful')
+    return req.json()
+
+
+# main method for validation testing the authentication
+if __name__ == "__main__":
+    print(f"Requesting token from {config['token_url']}, using client_id {config['client_id']}.")
+    token = get_token()
+    print(token)
