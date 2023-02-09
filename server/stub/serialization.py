39cbdb33fbd1067ea6b6389a0feb2b798d1c5930
Hacknerr <andrgart@stud.ntnu.no>
2023-02-09 13:39:58 +0100

    Added comprehensive documentation to functions in files deserialization.py and serialization.py. This to improve code readability and maintainability.

--- server/stub/serialization.py
+++ server/stub/serialization.py
@@ -3,18 +3,21 @@ import json
 from datetime import datetime
 
 
-# writes json data to .json file.
-# this method is currently deprecated.
+# Writes a list of dictionaries to a JSON file.
 def json_to_file(json_data):
+
+    # Adds a timestamp key and value to each dictionary in the list.
     for item in json_data:
         item['timestamp'] = datetime.now()
-    # writing to data.json.
+
+    # Writes the list of dictionaries to the file 'stub/data.json'.
     with open("stub/data.json", "w") as f:
         json.dump(json_data, f, indent=4, default=str)
 
 
-# writes json data to .json file.
+# Writes the provided JSON data to a file named "stub/area_data.json".
 def json_to_file_area(json_data):
-    # writing to data.json
+
+    # Writing to data.json.
     with open("stub/area_data.json", "w") as f:
         json.dump(json_data, f, indent=4, default=str)
