5abf01bf0ad910fc9959dcda7ea3b3a5229e009b
Hacknerr <andrgart@stud.ntnu.no>
2023-02-10 10:48:21 +0100

    Added a React frontend to repository. Implemented components folder with map-component.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+// app.js
+import Map from "./components/map.js";
+
+function App() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/map.js
@@ -0,0 +1,40 @@
+// map.js
+import React, { useState, useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const defaultLocation = [63.486112, 10.3980667];
+
+const Map = () => {
+  const [coordinates, setCoordinates] = useState([]);
+
+  useEffect(() => {
+    fetch("/coordinates")
+      .then((res) => res.json())
+      .then((data) => setCoordinates(data));
+  }, []);
+
+  return (
+    <MapContainer
+      style={{ height: "450px", width: "50%" }}
+      center={defaultLocation}
+      zoom={10}
+      scrollWheelZoom={true}
+    >
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png" />
+
+      {coordinates.map((coordinate, index) => (
+        <Marker key={index} position={coordinate}>
+          <Popup>
+            Latitude: {coordinate[0]} <br />
+            Longitude: {coordinate[1]}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Map;
