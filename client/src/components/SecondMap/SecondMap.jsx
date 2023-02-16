0118c3b8d0982580ce43070a94a1a82ab8e7d2b7
mmstene <mmstene@stud.ntnu.no>
2023-02-16 09:03:57 +0100

    Kept working on the SecondMap component. After research, useMapEvent was found, making it possible to do more with the Leaflet map. Currently, it flies to the location the user clicks, this will be improved. No data is gotten from the back-end yet, this is still in the works.

--- client/src/components/SecondMap/SecondMap.jsx
+++ client/src/components/SecondMap/SecondMap.jsx
@@ -2,16 +2,16 @@ import ReactDOMServer from 'react-dom/server';
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import {MapContainer, TileLayer, Popup, Marker} from "react-leaflet";
+import {MapContainer, TileLayer, Popup, Marker, useMapEvent} from "react-leaflet";
 import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 import Leaflet from "leaflet";
 import css from './SecondMap.module.css'
 import {useEffect, useState} from "react";
-import {Typography} from "@mui/material";
+import {Paper, Typography} from "@mui/material";
 
 const defaultLocation = [63.486112, 10.3980667]
 
-function SecondMap ({mode}) {
+function SecondMap({mode}) {
     const iconHTML = ReactDOMServer.renderToString(<DirectionsBoatIcon/>)
     const customMarkerIcon = new Leaflet.DivIcon({
         html: iconHTML,
@@ -56,46 +56,60 @@ function SecondMap ({mode}) {
         );
     }, []);
 
-    if (mode === true){
-        return <MapContainer
-            className={`${css.mapDark}`}
-            center={defaultLocation}
-            zoom={10}
-            scrollWheelZoom={false}
-        >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            <Marker position={[data.latitude, data.longitude]} icon={customMarkerIcon}>
-                <Popup>
-                    A marker which can show ship info <br /> Easily customizable with info and icons. <br/> Now in dark mode.
-                </Popup>
-            </Marker>
-        </MapContainer>
+    if (mode === true) {
+        return <Paper style={{height: "79vh"}}>
+            <MapContainer
+                className={`${css.mapDark}`}
+                center={defaultLocation}
+                zoom={10}
+                scrollWheelZoom={true}
+            >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                <Marker position={[data.latitude, data.longitude]} icon={customMarkerIcon}>
+                    <Popup>
+                        A marker which can show ship info <br/> Easily customizable with info and icons. <br/> Now in
+                        dark mode.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
+        </Paper>
     } else return (
+        <Paper style={{height: "79vh"}}>
             <MapContainer
                 className={`${css.map}`}
                 center={defaultLocation}
                 zoom={10}
-                scrollWheelZoom={false}
+                scrollWheelZoom={true}
             >
+                <MyComponent/>
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                 <Marker position={[63.437237, 10.395087]} icon={customMarkerIcon}>
                     <Popup>
                         <ReturnData data={data}/>
                     </Popup>
                 </Marker>
+                <Marker position={[63.566167, 9.898367]} icon={customMarkerIcon}>
+                    <Popup>
+                        Testing interval!
+                    </Popup>
+                </Marker>
             </MapContainer>
-        );
+        </Paper>
+    );
 }
+
 /** Alternative map solutions. They are noe free.
-http://leaflet-extras.github.io/leaflet-providers/preview/
-https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
+ http://leaflet-extras.github.io/leaflet-providers/preview/
+ https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
  https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png
  /*
 
-/**
- Function for returning data in the pop up window
+ /**
+ Function for returning data in the pop-up window
+ TODO: Make it so that this data is presented more beautiful and that the data is gotten from the back-end
  */
-function ReturnData({ data }) {
+function ReturnData({data}) {
     return (
         <Typography variant="body2">
             <ul>
@@ -112,6 +126,22 @@ function ReturnData({ data }) {
             </ul>
         </Typography>
     )
+}
+
+/**
+ * Uses the "useMapEvent" from react-leaflet to fly to a location on the map if that is clicked.
+ * TODO: Make it so it recognizes markers to fly to
+ * TODO: Make it so it goes to the next marker using an interval
+ * @constructor
+ */
+function MyComponent() {
+    const map = useMapEvent('click', () => {
+        map.on({
+            click: function (e) {
+                map.flyTo(e.latlng, 15)
+            }
+        });
+    })
 }
 
 export default SecondMap
