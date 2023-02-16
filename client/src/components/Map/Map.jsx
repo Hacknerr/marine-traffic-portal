c83164ef07025f1c9662a4c5be182c5aa101d85a
hacknerr <andrgart@stud.ntnu.no>
2023-02-16 16:33:12 +0100

    Added code-commenting to components in Map.jsx file

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -5,35 +5,45 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
-const SOCKET_URL = 'http://localhost:5000'; // replace with your socket URL
+const SOCKET_URL = 'http://localhost:5000';
 
 function Map() {
+  // State variable that holds an array of ship data
   const [ships, setShips] = useState([]);
 
   useEffect(() => {
+    // Connects to the WebSocket server using the given URL.
     const socket = io(SOCKET_URL, { transports: ['websocket'] });
 
+    // Event handler for when the client connects to the WebSocket server.
     socket.on('connect', function() {
        console.log('Client is now connected to server.');
        socket.emit('user_connected');
     });
 
-    socket.on('data', (data) => {
-      const shipData = JSON.parse(data);
-      setShips(shipData);
-      localStorage.setItem('ships', JSON.stringify(shipData));
-    });
-
+    // Event handler for when the WebSocket server disconnects.
     socket.on('disconnect', () => {
       console.log('WebSocket disconnected');
     });
 
+    // Event handler for receiving data from the WebSocket server.
+    socket.on('data', (data) => {
+      // Parses the received data as JSON.
+      const shipData = JSON.parse(data);
+      // Updates the state of the component with the received data.
+      setShips(shipData);
+      // Stores the received data in localStorage for caching.
+      localStorage.setItem('ships', JSON.stringify(shipData));
+    });
+
+    // Checks if there are cached ships in localStorage and load them into state if present.
     const storedShips = localStorage.getItem('ships');
     if (storedShips) {
       setShips(JSON.parse(storedShips));
     }
   }, []);
 
+  // Creates an array of Marker components based on the ships' data in state.
   const markers = ships.map((ship) => (
     <Marker key={ship.mmsi} position={[ship.latitude, ship.longitude]}>
       <Popup>
@@ -49,6 +59,7 @@ function Map() {
     </Marker>
   ));
 
+  // Renders a MapContainer component from the react-leaflet library, which contains the TileLayer and Markers.
   return (
     <MapContainer center={[63.4, 10.4]} zoom={10} style={{ height: "90vh", width: "100%" }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
