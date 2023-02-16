a0cc4a7f2e9778de03f422166a6664b9b3536105
hacknerr <andrgart@stud.ntnu.no>
2023-02-16 13:41:54 +0100

    Implemented real-time communication between the Python backend and the React frontend using flask_socketio

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,40 +1,60 @@
-// Map.jsx
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-const defaultLocation = [63.486112, 10.3980667];
+const SOCKET_URL = 'http://localhost:5000'; // replace with your socket URL
 
-const Map = () => {
-  const [coordinates, setCoordinates] = useState([]);
+function Map() {
+  const [ships, setShips] = useState([]);
 
   useEffect(() => {
-    fetch("/coordinates")
-      .then((res) => res.json())
-      .then((data) => setCoordinates(data));
+    const socket = io(SOCKET_URL, { transports: ['websocket'] });
+
+    socket.on('connect', function() {
+       console.log('Client is now connected to server.');
+       socket.emit('user_connected');
+    });
+
+    socket.on('data', (data) => {
+      const shipData = JSON.parse(data);
+      setShips(shipData);
+      localStorage.setItem('ships', JSON.stringify(shipData));
+    });
+
+    socket.on('disconnect', () => {
+      console.log('WebSocket disconnected');
+    });
+
+    const storedShips = localStorage.getItem('ships');
+    if (storedShips) {
+      setShips(JSON.parse(storedShips));
+    }
   }, []);
 
-  return (
-    <MapContainer
-      style={{ height: "450px", width: "50%" }}
-      center={defaultLocation}
-      zoom={10}
-      scrollWheelZoom={true}
-    >
-      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png" />
+  const markers = ships.map((ship) => (
+    <Marker key={ship.mmsi} position={[ship.latitude, ship.longitude]}>
+      <Popup>
+        <div>
+          <h2>{ship.name}</h2>
+          <p>MMSI: {ship.mmsi}</p>
+          <p>Ship Type: {ship.shipType}</p>
+          <p>Speed: {ship.speedOverGround} knots</p>
+          <p>Heading: {ship.trueHeading}°</p>
+          <p>Course: {ship.courseOverGround}°</p>
+        </div>
+      </Popup>
+    </Marker>
+  ));
 
-      {coordinates.map((coordinate, index) => (
-        <Marker key={index} position={coordinate}>
-          <Popup>
-            Latitude: {coordinate[0]} <br />
-            Longitude: {coordinate[1]}
-          </Popup>
-        </Marker>
-      ))}
+  return (
+    <MapContainer center={[63.4, 10.4]} zoom={10} style={{ height: "90vh", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers}
     </MapContainer>
   );
-};
+}
 
 export default Map;
--- server/server.py
+++ server/server.py
@@ -1,123 +1,116 @@
 import time
 import json
 import folium
-import threading
+
 from stub import deserialization, serialization
 from database import firestore
 from api import barentswatch, authentication
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
+from flask_socketio import SocketIO, emit, send
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-# Collects the MMSI values of all ships in a specific area from the Barentswatch API.
-# The parameter token is required to access the Barentswatch API.
 def stub(token):
-    # Collects the MMSI values of all ships in a specific area from the API.
     collected_data = barentswatch.data_request_of_area(token)
 
-    # If the collected data is not empty.
     if len(collected_data) != 0:
-        # Writes the collected MMSI values to a JSON file temporarily
         serialization.json_to_file_area(collected_data)
 
 
-# Extracts the latitude and longitude from a list of dictionaries.
-# After that, it returns a list of coordinates.
 def get_coordinates_from_list_of_dicts(dict_list):
     coordinates = []
     for dictionary in dict_list:
         if "latitude" in dictionary and "longitude" in dictionary:
-            # Extracts latitude and longitude and stores them as a list
             coordinates.append([dictionary["latitude"], dictionary["longitude"]])
-    # Returns a list of lists, where each list contains a latitude and longitude value.
     return coordinates
 
 
-# Adds markers to the Folium map based on the latitude and longitude
 def add_markers(m, coordinates_list):
     for coord in coordinates_list:
         folium.Marker(coord,
                       icon=folium.Icon(color='darkpurple', icon="ship", prefix='fa')
                       ).add_to(m)
-    # Returns a Folium map object with markers added.
     return m
 
 
-# Regenerates the map by updating the data displayed on it.
 def regenerate_map():
-    """
-    The following steps are performed:
-    1. Collects authentication token for BarentsWatch API.
-    2. Clears everything from the database.
-    3. Writes data from API to file.
-    4. Reads data from file and converts it to a list of MMSI.
-    5. Uses the MMSI list to fetch boat data from the API.
-    6. Writes the boat data to the database.
-    7. Collects the boat data from the database.
-    8. Extracts the latitude and longitude data from the boat data.
-    9. Adds markers to the map based on the latitude and longitude data.
-    10. Saves the map as an HTML file.
-    """
-
-    # Collects authentication token for BarentsWatch API.
     token = authentication.get_token()
 
-    # Clears everything from the database.
     firestore.delete_collections()
 
-    # Writes data from API to file.
     stub(token)
 
-    # Reads data from file and converts it to a list of MMSI.
     list_of_mmsi = json.loads(deserialization.read_file_to_string_area())
 
-    # Uses the MMSI list to fetch boat data from the API.
     boat_data_json_response = barentswatch.get_data_from_mmsi(token, list_of_mmsi)
 
-    # Writes the boat data to the database.
     firestore.write_to_firestore(boat_data_json_response)
 
-    # Collects the boat data from the database.
     list_of_boat_data = firestore.read_from_firestore()
 
-    # Extracts the latitude and longitude data from the boat data.
-    list_of_coords = get_coordinates_from_list_of_dicts(list_of_boat_data)
+    relevant_data = []
+    for boat in list_of_boat_data:
+        mmsi = boat.get("mmsi", "")
+        latitude = boat.get("latitude", "")
+        longitude = boat.get("longitude", "")
+        name = boat.get("name", "")
+        ship_type = boat.get("shipType", "")
+        speed_over_ground = boat.get("speedOverGround", "")
+        true_heading = boat.get("trueHeading", "")
+        course_over_ground = boat.get("courseOverGround", "")
+        relevant_data.append({
+            "mmsi": mmsi,
+            "latitude": latitude,
+            "longitude": longitude,
+            "name": name,
+            "shipType": ship_type,
+            "speedOverGround": speed_over_ground,
+            "trueHeading": true_heading,
+            "courseOverGround": course_over_ground
+        })
 
-    # Adds markers to the map based on the latitude and longitude data.
-    m = folium.Map(location=[63.486112, 10.3980667], zoom_start=11, tiles="cartodb positron", control_scale=True)
-    m = add_markers(m, list_of_coords)
-
-    # Saves the map as an HTML file.
-    m.save("map.html")
+    print("Map updated. Next update in a few minutes...")
+    return relevant_data
 
 
-# Initializes the Flask app.
-app = Flask(__name__)
+@socketio.event
+def connect():
+    print("I'm connected!")
 
 
-# Endpoint that returns a JSON object containing the coordinates of a location.
-@app.route('/coordinates', methods=['GET'])
-def get_coordinates():
-    # Hardcoded coordinates of a location.
-    coordinates = [63.486112, 10.3980667]
-
-    # Returns the coordinates in a JSON format.
-    return jsonify({'coordinates': coordinates})
+@socketio.event
+def connect_error():
+    print("The connection failed!")
 
 
-# This function is continuously executed every 2 minutes.
-# Allowing for the map to be updated in real-time.
-def update_map():
+@socketio.event
+def disconnect():
+    print('Im disconnected!')
+
+
+@socketio.on('data_request')
+def send_ship_data():
+    print("in data_request method")
+    payload = json.dumps(regenerate_map())
+    print(payload)
+    socketio.emit('data', payload)
+
+
+@socketio.on('user_connected')
+def on_user_connected():
     while True:
-        regenerate_map()
-        print("Map updated. Next update in a few minutes...")
-        time.sleep(120)
+        print("in user_connected method")
+        send_ship_data()
+        print("Sleeping...ZZzzzzz...")
+        socketio.sleep(30)
 
 
 # Main method
 if __name__ == "__main__":
-    # Starts a new thread for the `update_map` function
-    t = threading.Thread(target=update_map)
-    t.start()
-
-    # Runs the Flask app with debug mode set to False
-    app.run(debug=False)
+    socketio.run(app, debug=False, host='127.0.0.1')
