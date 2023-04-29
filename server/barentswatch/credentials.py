e70495879a02fa7d0262b5aa6b71edc0358ecb2e
hacknerr <andrgart@stud.ntnu.no>
2023-04-29 11:20:11 +0200

    Refractored code to read secrets file from correct path.

--- server/barentswatch/credentials.py
+++ server/barentswatch/credentials.py
@@ -1,14 +1,18 @@
+import os
 
-# Configuration dictionary for connecting to AIS API
+def get_secret_from_file(key, fallback=None):
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    secrets_file = os.path.join(this_dir, 'secrets.txt')
+    if os.path.exists(secrets_file):
+        with open(secrets_file) as f:
+            secrets = dict(line.strip().split('=', 1) for line in f if line.strip())
+        return secrets.get(key, fallback)
+    return fallback
+
 config = {
-    # Client ID for accessing the API
     'client_id': 'andrgart@stud.ntnu.no:ais-api-client',
-    # Client secret for accessing the API
-    'client_secret': 'secretkey1234',
-    # URL for getting the API access token
+    'client_secret': get_secret_from_file('client_secret', os.environ.get('CLIENT_SECRET')),
     'token_url': 'https://id.barentswatch.no/connect/token',
-    # Base URL for accessing the live AIS data
     'api_base_url': 'https://live.ais.barentswatch.no',
-    # Base URL for accessing historic AIS data
     'api_historic_base_url': 'https://historic.ais.barentswatch.no'
 }
