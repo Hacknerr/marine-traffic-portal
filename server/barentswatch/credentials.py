666f744884382d9ccacb466b42aa3e86a8c9d729
hacknerr <andrgart@stud.ntnu.no>
2023-04-30 10:13:10 +0200

    Fixed variable name in get_secret_from_file function

--- server/barentswatch/credentials.py
+++ server/barentswatch/credentials.py
@@ -1,12 +1,19 @@
+"""
+This module provides a function for retrieving secrets from a file and a configuration dictionary.
+"""
 import os
 
 
 def get_secret_from_file(key, fallback=None):
+    """
+    This function retrieves a secret value from a file named 'secrets.txt'
+    located in the same directory as the calling module.
+    """
     this_dir = os.path.dirname(os.path.abspath(__file__))
     secrets_file = os.path.join(this_dir, 'secrets.txt')
     if os.path.exists(secrets_file):
-        with open(secrets_file) as f:
-            secrets = dict(line.strip().split('=', 1) for line in f if line.strip())
+        with open(secrets_file) as file_handle:
+            secrets = dict(line.strip().split('=', 1) for line in file_handle if line.strip())
         return secrets.get(key, fallback)
     return fallback
 
