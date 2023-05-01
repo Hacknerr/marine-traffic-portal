f95b96a5b33349adbab1735cbc2a68986db1457d
hacknerr <andrgart@stud.ntnu.no>
2023-05-01 12:04:13 +0200

    Added unit tests for App and Sidebar components using Jest

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div>
+    <div data-testid="app">
       <Sidebar />
     </div>
   );
---
+++ client/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+// Mock the Sidebar component
+jest.mock('./components/Sidebar/Sidebar', () => {
+  // eslint-disable-next-line react/display-name
+  return () => <div data-testid="sidebar">Sidebar</div>;
+});
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+    const appElement = screen.getByTestId('app');
+    expect(appElement).toBeInTheDocument();
+  });
+
+  test('renders Sidebar component', () => {
+    render(<App />);
+    const sidebarElement = screen.getByTestId('sidebar');
+    expect(sidebarElement).toBeInTheDocument();
+  });
+});
